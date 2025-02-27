0e0081cf24c6710468dabdd91ef069b5107ed5b4
Ilene <ileneh889@yahoo.com>
2025-02-27 12:23:14 +0800

    add shadcn/ui and all UI components, net-themes | change favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-blue-500 font-bold">welcome to the corse!</h1>
+      <h1 className="text-blue-500">Welcome to the corse!</h1>
+      <h1 className="text-blue-500 font-barlow">Welcome to the corse!</h1>
+      <Button >Click here</Button>
     </div>
   );
 }
